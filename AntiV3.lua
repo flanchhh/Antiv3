e7f179e26a7fbe7e35794f760bb9378221b17b96
flanchhh <164205061+flanchhh@users.noreply.github.com>
2025-04-21 22:21:47 +0300

    Update AntiV3.lua

--- AntiV3.lua
+++ AntiV3.lua
@@ -1,3 +1,15 @@
-local player = game.Players.LocalPlayer
+local Players = game:GetService("Players")
+local StarterGui = game:GetService("StarterGui")
+local localPlayer = Players.LocalPlayer
 
-player:Kick("Please check the discord server | and get the latest link .gg.antiv2")
+setclipboard("https://discord.gg/antiv2")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "Script Disabled",
+    Text = "Join the Discord. Invite link copied to clipboard.",
+    Duration = 5
+})
+
+task.wait(2)
+
+localPlayer:Kick("Join antiv2. To get the latest script.  Invite link copied.")
